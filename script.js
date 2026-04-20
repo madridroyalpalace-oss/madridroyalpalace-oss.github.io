7df46e21862c34417b2b26a2340aa9aa4c2a4f16
madridroyalpalace-oss <madridroyalpalace@gmail.com>
2026-04-20 19:05:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const elements = document.querySelectorAll(".fade");
 
-const observer = new IntersectionObserver((entries) => {
+// animación scroll pro
+const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add("visible");
     }
   });
-}, {
-  threshold: 0.15
-});
+}, { threshold: 0.15 });
 
-elements.forEach(el => {
-  observer.observe(el);
+elements.forEach(el => observer.observe(el));
+
+// menú dinámico (aparece/desaparece)
+const nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    nav.style.background = "rgba(0,0,0,0.4)";
+  } else {
+    nav.style.background = "transparent";
+  }
 });
